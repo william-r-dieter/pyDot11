ed3886c98537f4b39ece419222a6d6a5f8761ceb
stryngs <info@ethicalreporting.org>
2017-08-21 23:44:24 -0400

    Cleanup

--- SRC/setup.py
+++ SRC/setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
     name = 'pyDot11',
-    version = '1.0.4',
+    version = '1.0.6',
     author = 'stryngs',
     author_email = 'info@ethicalreporting.org',
     packages = ['pyDot11', 'pyDot11.lib'],
     include_package_data = True,
-    url = 'https://github.com/stryngs/pyDot11',
+    url = 'https://github.com/ICSec/pyDot11',
     license ='GNU General Public License v3 or later (GPLv3+)',
     keywords = '802.11 wep wpa encryption decryption on-the-fly',
     description='Encrypt and Decrypt 802.11(WEP or WPA) on-the-fly'
